bddddf7f382572efae917faa30935f452063397a
Vikas Peddu <Vpeddu@uw.edu>
2019-10-28 17:53:41 -0700

    reproducibiliy scripts

--- reproducibility/HHV-6_reproducibility.sh
+++ reproducibility/HHV-6_reproducibility.sh
@@ -1,7 +1,7 @@
 #Workflow for generating CMV figures 
 
 #Bowtie2 alignment to CMV Merlin genome (NC_006273.2)
-bowtie2 -x <cmv_merlin_reference> -1 <r1.fastq> -2 <r2.fastq> --local --no-unal -p <cores> -S <aligned.sam> 
+bowtie2 -x <hhv-6_reference> -1 <r1.fastq> -2 <r2.fastq> --local --no-unal -p <cores> -S <aligned.sam> 
 
 #Convert SAM to BAM 
 samtools view -@ <cores> -Sb <aligned.sam> -o <aligned.bam> 
@@ -26,26 +26,18 @@ done > cmv_combined_masked_n_removed.fasta
 blastn -query cmv_combined_masked_n_removed.fasta -db /db/blast_db/nt -num_threads 42 -perc_identity 95 -evalue 1e-5 -out cmv_vs_full_nt.txt
 
 #Creates count table of BLAST hits to "Human hepesvirus 5" from the BLAST results.
-python blast_hits.py cmv_masked_blastn_out.txt 'Human herpesvirus 5'
+python blast_hits.py cmv_masked_blastn_out.txt 'Human herpesvirus 6<b/a>'
 
 #creates all_sample_data.csv- A table of sample name, FPM, FPKM, read counts, and classification (strong vs intermediate positive based on the FPM cutoff of .3)
 #file paths inside the script require editing based on how the above commands were run
 rscript --vanilla fpm_calculate.R
 
-#figure 1B
-rscript --vanilla figure_1b.r
-
-#run the first script to create a frequency table of insert sizes 
-samtools view -@ 40 <deduplicated_121r04_resequenced.bam> | awk '{print $9}' | sort | uniq -c > duplicates_removed_read_lengths.txt
-
 #for the scripts below file paths will have to be changed from the ones used on our local machine
-#figure 3
-rscript --vanilla figure_3.r
+#figure 5 inidividual figures
+rscript --vanilla figure_5A.r
+rscript --vanilla figure_5B.r
+rscript --vanilla figure_5C.r
 
-#figure 4 individual scripts
-rscript --vanilla figure_4A.r
-rscript --vanilla figure_4B.r
-rscript --vanilla figure_4C.r
+#figure 5 panel
+rscript --vanilla figure_5.r
 
-#figure 4 panel
-rscript --vanilla figure_4.r
