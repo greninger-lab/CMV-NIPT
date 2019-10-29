4e92a5f5f23fd173ea78213e8d00af557a33894d
Vikas Peddu <Vpeddu@uw.edu>
2019-10-29 13:59:36 -0700

    hhv6 rerun

--- reproducibility/CMV_reproducibility.sh
+++ reproducibility/CMV_reproducibility.sh
@@ -6,8 +6,11 @@ bowtie2 -x <cmv_merlin_reference> -1 <r1.fastq> -2 <r2.fastq> --local --no-unal 
 #Convert SAM to BAM 
 samtools view -@ <cores> -Sb <aligned.sam> -o <aligned.bam> 
 
+#sort BAM for picard
+samtools sort <aligned.bam> -o <sorted.bam> 
+
 #Deduplicate aligned bam file 
-picard MarkDuplicates I=<aligned.bam> O=<deduplicated.bam> M=<metrics.txt> REMOVE_DUPLICATES= TRUE ASSUME_SORTED=TRUE VALIDATION_STRINGENCY= SILENT
+picard MarkDuplicates I=<sorted.bam> O=<deduplicated.bam> M=<metrics.txt> REMOVE_DUPLICATES= TRUE ASSUME_SORTED=TRUE VALIDATION_STRINGENCY= SILENT
 
 #Only keep reads with 34 or more matches and extract reads for repeatmasking and BLAST later.
 ./34m.sh <deduplicated_bam_folder> 
